eabb3d11c4a8ea50557eea23b3ee830006699562
Bhairav <bhairavgotam066@gmail.com>
2023-10-17 14:29:44 +0530

    added codeIgniter framwork migration basics ui

---
+++ app/Database/Migrations/17102023558_admin.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Admim extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'phone' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'Password' => [
+                'type' => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'user_type' => [
+                'type' => 'ENUM("admin","user")',
+                "default"=> "user",
+                'null' => false,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('admin');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('admin');
+    }
+}
